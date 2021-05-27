610734c2807dd3c978a8e839f0da53f3dde88198
isaya1910 <Oazis123456>
2021-05-27 19:01:02 +0600

    Linked list

--- mytest1.py
+++ mytest1.py
@@ -41,31 +41,40 @@ class LinkedList:
             node = node.next
         return ans  # здесь будет ваш код
 
-    def delete(self, val, all=False):
-        node = self.head
-        prev = None
-        while node is not None and node.value == val:
-            self.head = node.next
-            node = self.head
-            if not all: break
-
-        while node is not None:
-            while node is not None and node.value != val:
-                prev = node
-                node = node.next
-            if node is None: break
-            prev.next = node.next
-            node = prev.next
-            if not all: break
+    def update_tail(self):
         node = self.head
         self.tail = self.head
         while node is not None:
             self.tail = node
             node = node.next
 
+    def delete(self, val, all=False):
+        node = self.head
+        prev = None
+        while node is not None and node.value == val:
+            self.head = node.next
+            node = self.head
+            if not all:
+                self.update_tail()
+                return
+
+        while node is not None:
+            while node is not None and node.value != val:
+                prev = node
+                node = node.next
+            if node is None:
+                self.update_tail()
+                return
+            prev.next = node.next
+            node = prev.next
+            if not all:
+                self.update_tail()
+                return
+
     def clean(self):
         while self.head is not None:
             self.head = self.head.next
+        self.tail = None
 
     def len(self):
         ans = 0
@@ -92,16 +101,24 @@ class LinkedList:
 
 def delete_test():
     linked_list = LinkedList()
-    linked_list.add_in_tail(Node(1))
 
-    linked_list.add_in_tail(Node(3))
-    linked_list.add_in_tail(Node(3))
     linked_list.add_in_tail(Node(5))
-    linked_list.add_in_tail(Node(5))
+    linked_list.add_in_tail(Node(7))
     linked_list.add_in_tail(Node(5))
 
-    linked_list.delete(5, True)
     linked_list.print_all_nodes()
-    assert linked_list.head.value == 1
+    linked_list.delete(5, False)
+    linked_list.print_all_nodes()
+   # assert linked_list.head is None
 
-    assert linked_list.tail.value == 3
+    #assert linked_list.tail is None
+
+def clean_test():
+    linked_list = LinkedList()
+    linked_list.add_in_tail(Node(5))
+    linked_list.clean()
+    linked_list.clean()
+    assert linked_list.len() == 0
+    assert linked_list.head is None
+    assert linked_list.tail is None
+
