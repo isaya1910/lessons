bce468e149e02e6da52bb945febe1e731fcac14e
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-02 15:21:09 +0600

    Lesson 1(LinkedList)

--- task_1_8_unit_test.py
+++ task_1_8_unit_test.py
@@ -5,6 +5,18 @@ from task_1_8 import task1_8
 
 
 class MyTestCase(unittest.TestCase):
+    def test_task1_8_when_different_lens(self):
+        linked_list1 = LinkedList()
+        linked_list1.add_in_tail(Node(6))
+        linked_list1.add_in_tail(Node(7))
+        linked_list1.add_in_tail(Node(9))
+
+        linked_list2 = LinkedList()
+        linked_list2.add_in_tail(Node(6))
+        ans = task1_8(linked_list1, linked_list2)
+
+        self.assertIs(ans, None)
+
     def test_task1_8(self):
         linked_list1 = LinkedList()
         linked_list1.add_in_tail(Node(6))
@@ -20,7 +32,3 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(ans.head.value, 12)
         self.assertEqual(ans.head.next.value, 14)
         self.assertEqual(ans.head.next.next.value, 10)
-
-        linked_list1.add_in_tail(Node(5))
-        ans = task1_8(linked_list1, linked_list2)
-        self.assertIs(ans, None)
