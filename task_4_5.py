7d2ddc5f81f1109c252c94d09fce5bba935871e2
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-04 14:37:45 +0600

    Scopes and polish notation

--- task_4_5.py
+++ task_4_5.py
@@ -16,26 +16,26 @@ def is_scopes_balance(scopes):
 
 
 def polish_notation(notation):
-    stack2 = Stack()
+    stack = Stack()
     for c in notation:
         if c.isdigit():
-            stack2.push(int(c))
+            stack.push(int(c))
 
         if c == '+':
-            digit1 = stack2.pop()
-            digit2 = stack2.pop()
-            stack2.push(digit1 + digit2)
+            digit1 = stack.pop()
+            digit2 = stack.pop()
+            stack.push(digit1 + digit2)
         if c == '-':
-            digit1 = stack2.pop()
-            digit2 = stack2.pop()
-            stack2.push(digit1 - digit2)
+            digit1 = stack.pop()
+            digit2 = stack.pop()
+            stack.push(digit1 - digit2)
         if c == '*':
-            digit1 = stack2.pop()
-            digit2 = stack2.pop()
-            stack2.push(digit1 * digit2)
+            digit1 = stack.pop()
+            digit2 = stack.pop()
+            stack.push(digit1 * digit2)
         if c == '/':
-            digit1 = stack2.pop()
-            digit2 = stack2.pop()
-            stack2.push(digit1 / digit2)
+            digit1 = stack.pop()
+            digit2 = stack.pop()
+            stack.push(digit1 / digit2)
 
-    return stack2.pop()
+    return stack.pop()
--- unit_test_4_5.py
+++ unit_test_4_5.py
@@ -5,20 +5,6 @@ from task_4_5 import is_scopes_balance, polish_notation
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-    def test_stack(self):
-        stack = Stack()
-        stack.push(123)
-        stack.push(455)
-        self.assertEqual(stack.pop(), 455)
-        self.assertEqual(stack.pop(), 123)
-        stack.push(11)
-        self.assertEqual(stack.peek(), 11)
-        self.assertEqual(stack.size(), 1)
-        self.assertEqual(stack.pop(), 11)
-        self.assertEqual(stack.pop(), None)
 
     def test_stack_scopes(self):
         test_string = "()(())"
