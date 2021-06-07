344fc0532f0139eb4de0114d7f46029ff3b6154a
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-07 15:46:15 +0600

    Deque

---
+++ mytest6.py
@@ -0,0 +1,23 @@
+class Deque:
+    def __init__(self):
+        self.list = []
+
+    def addFront(self, item):
+        self.list.insert(0, item)
+
+    def addTail(self, item):
+        self.list.append(item)
+
+    def removeFront(self):
+        if len(self.list) == 0:
+            return None
+        return self.list.pop(0)
+
+    def removeTail(self):
+        if len(self.list) == 0:
+            return None
+
+        return self.list.pop()  # если очередь пуста
+
+    def size(self):
+        return len(list)  # размер очереди
---
+++ unit_test_6.py
@@ -0,0 +1,23 @@
+import unittest
+
+from mytest6 import Deque
+
+
+class MyTestCase(unittest.TestCase):
+    def test_deque(self):
+        deque = Deque()
+        deque.addTail(1)
+        deque.addTail(2)
+        deque.addFront(3)
+
+        self.assertEqual(deque.removeFront(), 3)
+        self.assertEqual(deque.removeFront(), 1)
+        self.assertEqual(deque.removeFront(), 2)
+        self.assertEqual(deque.removeTail(), None)
+
+        deque.addTail(6)
+        deque.addTail(5)
+        deque.addTail(4)
+        deque.addTail(2)
+        self.assertEqual(deque.removeFront(), 6)
+        self.assertEqual(deque.removeTail(), 2)
