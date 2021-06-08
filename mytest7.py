7be7c18220426e63dada008afd8e24a55169f54f
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-08 23:41:31 +0600

    Ordered list

---
+++ mytest7.py
@@ -0,0 +1,144 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class OrderedList:
+    def __init__(self, asc):
+        self.head = None
+        self.tail = None
+        self.__ascending = asc
+        self.size = 0
+
+    def compare(self, v1, v2):
+        if v1 < v2: return -1
+        if v1 > v2: return 1
+        return 0
+
+    def add_tail(self, node):
+        temp = self.tail
+        self.tail = node
+        node.prev = temp
+        temp.next = node
+
+    def add_head(self, node):
+        temp = self.head
+        self.head = node
+        node.next = temp
+        temp.prev = node
+
+    def add(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+        elif self.head is not None and self.tail is None:
+            if self.compare(node.value, self.head.value) == 1 or self.compare(node.value, self.head.value) == 0:
+                if self.__ascending:
+                    node.prev = self.head
+                    self.head.next = node
+                    self.tail = node
+                else:
+                    temp = self.head
+                    node.next = temp
+                    temp.prev = node
+                    self.head = node
+                    self.tail = temp
+            else:
+                if self.__ascending:
+                    temp = self.head
+                    node.next = temp
+                    temp.prev = node
+                    self.head = node
+                    self.tail = temp
+                else:
+                    node.prev = self.head
+                    self.head.next = node
+                    self.tail = node
+        elif self.head is not None and self.tail is not None:
+            if self.__ascending:
+                it = self.tail
+                while it is not None and (self.compare(it.value, node.value) == 1 or self.compare(it.value,
+                                                                                                  node.value) == 0):
+                    it = it.prev
+                if it is None:
+                    self.add_head(node)
+                elif it is self.tail:
+                    self.add_tail(node)
+                else:
+                    temp = it
+                    node.prev = temp
+                    node.next = temp.next
+                    temp.next.prev = node
+                    temp.next = node
+
+            else:
+                it = self.head
+                while it is not None and (
+                        self.compare(it.value, node.value) == 1 or self.compare(it.value, node.value) == 0):
+                    it = it.next
+                if it is None:
+                    self.add_tail(node)
+                elif it is self.head:
+                    self.add_head(node)
+                else:
+                    temp = it
+                    node.next = temp
+                    node.prev = temp.prev
+                    temp.prev.next = node
+                    temp.prev = node
+        self.size += 1
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def update_tail(self):
+        node = self.head
+        self.tail = self.head
+        while node is not None:
+            self.tail = node
+            node = node.next
+
+    def delete(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                prev = node.prev
+                next = node.next
+                if prev is not None:
+                    prev.next = next
+                    if next is not None:
+                        next.prev = prev
+                else:
+                    self.head = next
+                    if next is not None:
+                        next.prev = None
+                self.size -= 1
+                break
+            node = node.next
+        self.update_tail()
+
+    def clean(self, asc):
+
+        self.__ascending = asc
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def len(self):
+        return self.size  # здесь будет ваш код
+
+    def get_all(self):
+
+        r = []
+        node = self.head
+        while node != None:
+            r.append(node)
+            node = node.next
+        return r
---
+++ unit_test_7.py
@@ -0,0 +1,89 @@
+import unittest
+
+from mytest7 import OrderedList
+from random import randrange
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_ordered_list_increasing_order(self):
+        ordered_list = OrderedList(True)
+        n = 100
+        list = []
+        for i in range(n):
+            integer = randrange(1000)
+            list.append(integer)
+            ordered_list.add(integer)
+        list.sort()
+        list_from_ordered_list = ordered_list.get_all()
+
+        for i in range(n):
+            self.assertEqual(list[i], list_from_ordered_list[i].value)
+
+    def test_ordered_list_decreasing_order(self):
+        ordered_list = OrderedList(False)
+        n = 100
+        list = []
+        for i in range(n):
+            integer = randrange(1000)
+            list.append(integer)
+            ordered_list.add(integer)
+        list.sort(reverse=True)
+        list_from_ordered_list = ordered_list.get_all()
+
+        for k in range(n):
+            self.assertEqual(list[k], list_from_ordered_list[k].value)
+
+    def test_ordered_list_delete(self):
+        ordered_list = OrderedList(False)
+        n = 100
+        list = []
+        for i in range(n):
+            integer = randrange(1000)
+            list.append(integer)
+            ordered_list.add(integer)
+        list.sort(reverse=True)
+        list_from_ordered_list = ordered_list.get_all()
+        for k in range(n):
+            self.assertEqual(list[k], list_from_ordered_list[k].value)
+
+        ordered_list.delete(list[25])
+
+        list.remove(list[25])
+        ordered_list.delete(list[0])
+        list.remove(list[0])
+
+        list_from_ordered_list = ordered_list.get_all()
+
+        for k in range(len(list)):
+            self.assertEqual(list[k], list_from_ordered_list[k].value)
+
+    def test_ordered_list_len(self):
+        ordered_list = OrderedList(False)
+        n = 1000
+
+        list_to_delete = []
+        for i in range(n):
+            integer = randrange(1000)
+            ordered_list.add(integer)
+            if i % 3 == 0:
+                list_to_delete.append(integer)
+        self.assertEqual(ordered_list.len(), n)
+
+        for i in range(len(list_to_delete)):
+            ordered_list.delete(list_to_delete[i])
+        self.assertEqual(ordered_list.len(), n - len(list_to_delete))
+        ordered_list.clean(False)
+        self.assertEqual(ordered_list.len(), 0)
+
+        n = 100
+        list = []
+        for i in range(n):
+            integer = randrange(1000)
+            list.append(integer)
+            ordered_list.add(integer)
+        list.sort(reverse=True)
+        list_from_ordered_list = ordered_list.get_all()
+
+        for k in range(n):
+            self.assertEqual(list[k], list_from_ordered_list[k].value)
