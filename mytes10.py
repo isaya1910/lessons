57cb16b10e9cec024a7b4f9028df57b1815d0a99
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-15 00:08:59 +0600

    Set

---
+++ mytes10.py
@@ -0,0 +1,56 @@
+# наследуйте этот класс от HashTable
+# или расширьте его методами из HashTable
+class PowerSet:
+
+    def __init__(self):
+        self.dict = {}
+
+    def size(self):
+        return len(self.dict.keys())
+        # количество элементов в множестве
+
+    def print(self):
+        print(*self.dict.keys())
+
+    def put(self, value):
+        self.dict[value] = value
+
+    def get(self, value):
+        return self.dict.get(value) is not None
+
+    def remove(self, value):
+        # возвращает True если value удалено
+        return self.dict.pop(value) is not None
+
+    def intersection(self, set2):
+        ans = PowerSet()
+        for key in self.dict.keys():
+            if set2.get(key):
+                ans.put(key)
+        return ans
+
+    def union(self, set2):
+        ans = PowerSet()
+        for key in self.dict.keys():
+            ans.put(key)
+
+        for key in set2.dict.keys():
+            ans.put(key)
+        return ans
+
+    def difference(self, set2):
+        # разница текущего множества и set2
+        ans = PowerSet()
+        for key in self.dict.keys():
+            if not set2.get(key):
+                ans.put(key)
+        return ans
+
+    def issubset(self, set2):
+        # возвращает True, если set2 есть
+        # подмножество текущего множества,
+        # иначе False
+        for key in set2.dict.keys():
+            if not self.get(key):
+                return False
+        return True
---
+++ unit_test_10.py
@@ -0,0 +1,58 @@
+import unittest
+
+from mytes10 import PowerSet
+
+
+class MyTestCase(unittest.TestCase):
+    def test_set(self):
+        set = PowerSet()
+        self.assertEqual(set.get("asd"), False)
+        set.put("asd")
+        self.assertEqual(set.get("asd"), True)
+        set.remove("asd")
+        self.assertEqual(set.get("asd"), False)
+        set.put("sss")
+        self.assertEqual(set.size(), 1)
+
+    def test_4(self):
+        a = {1}
+        b = {1,2,3}
+        print(b.issubset(a))
+        print(a.issubset(b))
+        a = set()
+        print(a.issubset(b))
+        print(b.issubset(a))
+
+    def test_3_set(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set1.put(3)
+        self.assertEqual(set1.issubset(set2), True)
+        self.assertEqual(set2.issubset(set1), False)
+
+        set1.union(set2).print()
+        set2.put(3)
+        set1.difference(set2).print()
+        self.assertEqual(set1.issubset(set2), True)
+        self.assertEqual(set2.issubset(set1), True)
+
+
+    def test_2_set(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        for i in range(100):
+            if i % 3 == 0:
+                set1.put(4)
+            else:
+                set1.put(6)
+        for i in range(50):
+            if i % 3 == 0:
+                set2.put(123)
+            else:
+                set2.put(4)
+
+        set1.difference(set2).print()
+        set1.intersection(set2).print()
+        set1.union(set2).print()
+        self.assertEqual(set1.issubset(set2), False)
+        self.assertEqual(set2.issubset(set1), False)
